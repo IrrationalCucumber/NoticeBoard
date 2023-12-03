b66ef55aa2cb4c56a8d6a2a6d49b0a9b17c8b568
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-12-03 14:52:55 +0800

    added error message function for signin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -3,12 +3,25 @@ import { Link } from "react-router-dom";
 
 function SignIn() {
   const [account, setAccount] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  //hndle event when button is click
+  const handleClick = () => {
+    try {
+      const res = axios.get("");
+      if (res.data == null) {
+        setErrorMessage = "Account does not exist";
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
       <input type="username" placeholder="username" name="username" />
       <input type="password" placeholder="password" name="password" />
       <p>{errorMessage}</p>
-      <button>Sign In</button>
+      <button onClick={handleClick}>Sign In</button>
       <h4>
         Don't have an Account?Sign-up<Link to="/sign-up">here</Link>
       </h4>

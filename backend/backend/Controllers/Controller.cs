ee4c58e6f71f7b416892f942da764f8f105e010a
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-12-08 21:17:52 +0800

    backend setup

---
+++ backend/backend/Controllers/Controller.cs
@@ -0,0 +1,80 @@
+using backend.Views;
+using backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [Route("[action]")]
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+        [HttpGet]
+        public List<string> Sample()
+        {
+            List<string> list = new List<string> { "first", "second", "third", "fourth", "fifth", "sixth" };
+
+            return list;
+        }
+
+
+        [HttpPost]
+        public string Signup(userModel model)
+        {
+            using (var context = new dbContext())
+            {
+                context.Users.Add(new user
+                {
+                    Username = model.Username,
+                    Password = model.Password,
+                    FName = model.FName,
+                    LName = model.LName,
+                    Email = model.Email,
+                    Age = model.Age,
+                    Gender = model.Gender,
+                });
+
+                context.SaveChanges();
+            }
+            return "SUCCESS";
+        }
+
+
+        [HttpGet]
+        public ActionResult<userModel> Profile(int userId)
+        {
+            using (var context = new dbContext())
+            {
+                var user = context.Users.FirstOrDefault(user => user.Id == userId);
+
+                if (user != null)
+                {
+                    return Ok(user);
+                }
+
+                return NotFound();
+            }
+        }
+
+        //for sign in
+        //verify if user exist
+        [HttpGet]
+        public ActionResult<int> SignIn(string username, string password)
+        {
+            using (var context = new dbContext())
+            {
+                var user = context.Users.FirstOrDefault(user => user.Username == username && user.Password == password);
+
+                if (user != null)
+                {
+                    return Ok(user.Id);
+                }
+
+                return NotFound();
+            }
+        }
+
+
+    }
+}
---
+++ backend/backend/Views/dbContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Views;
+
+public partial class dbContext : DbContext
+{
+    public dbContext()
+    {
+    }
+
+    public dbContext(DbContextOptions<dbContext> options)
+        : base(options)
+    {
+    }
+    //public virtual DbSet<Transaction> Transactions { get; set; }
+
+    public virtual DbSet<user> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("server=localhost;uid=admin;pwd=admin;database=abdb"); //<- change the login credentials according to your mysql connection account in your pc
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //save the info on the entered data to the db
+        //for user account only
+        modelBuilder.Entity<user>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("useraccount");
+
+            entity.Property(e => e.Id).HasColumnName("userID");
+            entity.Property(e => e.FName)
+                .HasMaxLength(45)
+                .HasColumnName("userFname");
+            entity.Property(e => e.LName)
+                .HasMaxLength(45)
+                .HasColumnName("userLname");
+            entity.Property(e => e.Password)
+                .HasMaxLength(45)
+                .HasColumnName("password");
+            entity.Property(e => e.Username)
+                .HasMaxLength(45)
+                .HasColumnName("username");
+            entity.Property(e => e.Email)
+                .HasMaxLength(45)
+                .HasColumnName("userEmail");
+            entity.Property(e => e.Age).HasColumnName("userAge");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(45)
+                .HasColumnName("userGender");
+            entity.Property(e => e.Type)
+                .HasMaxLength(45)
+                .HasColumnName("userType");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}

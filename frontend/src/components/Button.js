d30846099e5288f74e65aad4c633f50698d79c38
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-12-10 14:47:45 +0800

    fixed button link

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,32 +1,34 @@
-import React from 'react'
-import './Button.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Button.css";
+import { Link } from "react-router-dom";
 
-const STYLES = ['btn--primary', 'btn--outline']
+const STYLES = ["btn--primary", "btn--outline"];
 
-const SIZES = ['btn--medium', 'btn--large']
+const SIZES = ["btn--medium", "btn--large"];
 
 //render anything inside the button
 export const Button = ({
-    children,
-     type, 
-     onClick, 
-     buttonStyle, 
-     buttonSize
-    }) => {
-        const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
 
-        const checkButtonSize = SIZES.includes(buttonSize) ?  buttonSize : SIZES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-        return (
-            <Link to='/sign-up' className='btn--mobile'>
-                <button
-                className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-                onClick={onClick}
-                type={type}
-                >
-                    {children}
-                </button>
-            </Link>
-        )
-    };
+  return (
+    <Link to="/" className="btn--mobile">
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};

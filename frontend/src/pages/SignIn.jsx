4362d01345b7e30abff1f0e612ccbce53f3569e5
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-12-10 10:53:21 +0800

    added csss design

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./style.css";
 
 function SignIn() {
   const [username, setUsername] = useState("");
@@ -13,50 +14,52 @@ function SignIn() {
   const handleClick = async (e) => {
     e.preventDefault();
 
-    //   // Simple validation for empty username or password
-    //   if (!username || !password) {
-    //     setErrorMessage("Please enter both username and password.");
-    //     return;
-    //   }
-    //   axios
-    //     .get(
-    //       `https://localhost:8800/SignIn?username=${username}&password=${password}`
-    //     )
-    //     .then((response) => {
-    //       if (response != null) {
-    //         //console.log(response.data);
-    //         //setUserID(response.data);
-    //         //console.log(userID);
-    //         navigate(`/profile/${userID}`);
-    //       } else {
-    //         setErrorMessage("Invalid Username/Password");
-    //       }
-    //     })
-    //     .catch(function (error) {
-    //       console.log(error);
-    //       setErrorMessage("Invalid Username/Password");
-    //     });
+    // Simple validation for empty username or password
+    if (!username || !password) {
+      setErrorMessage("Please enter both username and password.");
+      return;
+    }
+    axios
+      .get(
+        `https://localhost:8800/SignIn?username=${username}&password=${password}`
+      )
+      .then((response) => {
+        if (response != null) {
+          //console.log(response.data);
+          setUserID(response.data);
+          //console.log(userID);
+          navigate(`/home/${userID}`);
+        } else {
+          setErrorMessage("Invalid Username/Password");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+        setErrorMessage("Invalid Username/Password");
+      });
   };
 
   return (
     <div>
-      <input
-        type="username"
-        placeholder="username"
-        name="username"
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="password"
-        name="password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <p>{errorMessage}</p>
-      <button onClick={handleClick}>Sign In</button>
-      <h4>
-        Don't have an Account?Sign-up<Link to="/sign-up">here</Link>
-      </h4>
+      <form action="" className="form_si">
+        <input
+          type="username"
+          placeholder="username"
+          name="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p className="error">{errorMessage}</p>
+        <button onClick={handleClick}>Sign In</button>
+        <h4>
+          Don't have an Account?Sign-up <Link to="/sign-up">here!</Link>
+        </h4>
+      </form>
     </div>
   );
 }

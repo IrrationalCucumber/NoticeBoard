25b45266c3725938771070416c14659a1dd92b53
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-12-10 23:47:07 +0800

    added notice list page

---
+++ frontend/src/pages/NoticeList.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+
+function NoticeList() {
+  const [noitces, setNotices] = useState([]);
+  //carry id to other page
+  const location = useLocation();
+  const userID = location.pathname.split("/")[2]; //pathname to array from url
+
+  //get all notices based on userid
+  useEffect(() => {
+    const fetchAllNotice = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:8800/commission/${userID}`
+        );
+        console.log(res.data);
+        setNotices(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllNotice();
+  }, []);
+
+  return (
+    <div>
+      <h1>Your 'Notice' to Senpai List</h1>
+      {noitces.map((Notice) => (
+        <div key={Notice.postID}>
+          <p>{Notice.Title}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default NoticeList;

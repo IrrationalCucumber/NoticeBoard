52522b40253569285261ed627e8995c72b5cdcf1
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-12-15 15:49:04 +0800

    map frontend

--- frontend/src/components/MapContainer.jsx
+++ frontend/src/components/MapContainer.jsx
@@ -2,8 +2,29 @@ import React, { useRef, useEffect, useState } from "react";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./map.css";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Map() {
+  //carry id to other page
+  const location = useLocation();
+  const userID = location.pathname.split("/")[2]; //pathname to array from url
+  //view post
+  const navigate = useNavigate();
+  const viewPost = (id) => {
+    navigate(`/view/${userID}/${id}`);
+  };
+  //get all posted noitce
+  const fetchNotices = async () => {
+    try {
+      const response = await fetch("https://localhost:8800/AllNotice");
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng] = useState(123.8854);
@@ -19,6 +40,19 @@ export default function Map() {
       style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
       center: [lng, lat],
       zoom: zoom,
+    });
+    // add markers for every notice
+    fetchNotices().then((notices) => {
+      notices.forEach((notice) => {
+        const marker = new maplibregl.Marker({ color: "#FF0000" }) // Red marker for commissions
+          .setLngLat([notice.long, notice.lat])
+          .setPopup(
+            new maplibregl.Popup().setHTML(
+              `<h3>${notice.title}</h3><p>${notice.date}</p>`
+            )
+          )
+          .addTo(map.current);
+      });
     });
 
     map.current.addControl(new maplibregl.NavigationControl(), "top-right");
--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MapContainer from "../components/MapContainer";
+import Navbar from "../components/Navbar";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 function Map() {
+  //carry id to other page
+  const location = useLocation();
+  const userID = location.pathname.split("/")[2]; //pathname to array from url
+  //get name
+  const [name, setName] = useState({
+    username: "",
+  });
+  useEffect(() => {
+    const fetchName = async () => {
+      axios
+        .get(`https://localhost:8800/GetName?userId=${userID}`)
+        .then((response) => {
+          //console.log(response.data);
+          setName({
+            username: response.data,
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+    fetchName();
+  }, [userID]);
   return (
     <div>
-      THIS IS MAP
+      <Navbar
+        home={`/home/${userID}`}
+        notices={`/notices/${userID}`}
+        map={`/map/${userID}`}
+        page2="NOTICES"
+        page4={name.username}
+        profile={`/profile/${userID}`}
+      />
+
       <MapContainer />
     </div>
   );
